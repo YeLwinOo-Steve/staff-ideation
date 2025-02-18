994763494573cef872104b48e5063feb46f165d7
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-02-18 12:01:43 +0700

    added: forget password success page

---
+++ app/forget_password/success/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ForgetPasswordSuccessPage = () => {
+    return (
+        <div>Forget Password Success Page</div>
+    )
+}
+
+export default ForgetPasswordSuccessPage
