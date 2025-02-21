3fd3545ab8ac9110813dec733edfad7bf03635d9
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-02-21 12:54:51 +0700

    updated: ts jest config

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  preset: 'ts-jest',
+};
+
+export default config;
