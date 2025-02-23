69218415ded551b4206b0f6647fa97e604278c20
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-02-23 19:19:43 +0700

    added: loading for zip download

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,40 +2,46 @@
 
 import { useAuthStore } from "@/store/authStore"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { CSVLink } from "react-csv"
 import JSZip from "jszip";
 import { saveAs } from "file-saver"
 
 const zip = new JSZip();
-const downloadZipFile = async () => {
-  const urls = [
-    'https://i.pinimg.com/736x/7e/f3/00/7ef3009e0efeb251f1d6d16f56ddff64.jpg',
-    'https://i.pinimg.com/236x/ac/0d/b7/ac0db732637f7a08231ea4cd23d411a9.jpg',
-  ];
 
-  const promises = urls.map(async (url, index) => {
-    const proxyUrl = "https://api.allorigins.win/raw?url=";
-    const res = await fetch(proxyUrl + encodeURIComponent(url));
-    const blob = await res.blob();
-    zip.file(`file${index + 1}.${blob.type.split("/")[1]}`, blob);
-  });
-  await Promise.all(promises);
+const ZipDownloadBtn = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  
+  const downloadZipFile = async () => {
+    const urls = [
+      'https://i.pinimg.com/736x/7e/f3/00/7ef3009e0efeb251f1d6d16f56ddff64.jpg',
+      'https://i.pinimg.com/236x/ac/0d/b7/ac0db732637f7a08231ea4cd23d411a9.jpg',
+    ];
+    setIsLoading(true);
+    const promises = urls.map(async (url, index) => {
+      const proxyUrl = "https://api.allorigins.win/raw?url=";
+      const res = await fetch(proxyUrl + encodeURIComponent(url));
+      const blob = await res.blob();
+      zip.file(`file${index + 1}.${blob.type.split("/")[1]}`, blob);
+    });
+    await Promise.all(promises);
+  
+    const zipBlob = await zip.generateAsync({ type: 'blob' });
+    saveAs(zipBlob, 'downloaded_documents.zip');
+    setIsLoading(false);
+  }
 
-  const zipBlob = await zip.generateAsync({ type: 'blob' });
-  saveAs(zipBlob, 'downloaded_documents.zip');
-}
-
-const ZipDownloadBtn = () => (
-  <div>
-    <button
-      onClick={downloadZipFile}
-      className="btn btn-outline mb-8"
-    >
-      Download Zip Files
-    </button>
-  </div>
-);
+  return (
+    <div>
+      <button
+        onClick={downloadZipFile}
+        className="btn btn-outline mb-8" disabled={isLoading}
+      >
+        {isLoading ? <span className="loading loading-spinner"></span> : `Download Zip Files`}
+      </button>
+    </div>
+  )
+};
 
 export default function Dashboard() {
   const user = useAuthStore((state) => state.user)
