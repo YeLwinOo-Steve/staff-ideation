81d2e664b14a52131fe0876f6c63afd9d56d68d5
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-02-24 00:27:14 +0700

    added: cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ const zip = new JSZip();
 
 const ZipDownloadBtn = () => {
   const [isLoading, setIsLoading] = useState(false)
-  
+
   const downloadZipFile = async () => {
     const urls = [
       'https://i.pinimg.com/736x/7e/f3/00/7ef3009e0efeb251f1d6d16f56ddff64.jpg',
@@ -25,7 +25,7 @@ const ZipDownloadBtn = () => {
       zip.file(`file${index + 1}.${blob.type.split("/")[1]}`, blob);
     });
     await Promise.all(promises);
-  
+
     const zipBlob = await zip.generateAsync({ type: 'blob' });
     saveAs(zipBlob, 'downloaded_documents.zip');
     setIsLoading(false);
@@ -69,7 +69,22 @@ export default function Dashboard() {
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Welcome, {user.name}!</h1>
           <p className="py-6">You have successfully logged in to your dashboard.</p>
-
+          <div className="stats-horizontal">
+            <div className="stats shadow mx-4 my-8">
+              <div className="stat">
+                <div className="stat-title">Total Ideas</div>
+                <div className="stat-value">25</div>
+                <div className="stat-desc">Number of unique ideas</div>
+              </div>
+            </div>
+            <div className="stats shadow mx-4">
+              <div className="stat">
+                <div className="stat-title">Total Staff</div>
+                <div className="stat-value">18</div>
+                <div className="stat-desc">Staff in EWSD Dep</div>
+              </div>
+            </div>
+          </div>
           <div className="mb-8">
             <CSVLink data={data} filename="data.csv" className="btn btn-link">
               Download CSV
