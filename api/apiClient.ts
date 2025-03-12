1bbb20717ecd88461dc95b0143d5c0327f6d5693
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-03-12 13:02:36 +0700

    added: api client with axios

---
+++ api/apiClient.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: process.env.BASE_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message = error.response?.data?.message || "Something went wrong";
+    return Promise.reject(error);
+  },
+);
+
+export default apiClient;
