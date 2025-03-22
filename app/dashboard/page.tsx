4b059b261ffe6b4b0fa4ebbe04e4d04667d9ed74
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-03-22 10:54:05 +0700

    updated: file import

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { useAuthStore } from "@/store/authStore";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import JSZip from "jszip";
-import FilePreview from "@/app/dashboard/components/file_preview";
+import FilePreview from "@/app/dashboard/components/filePreview";
 import { saveAs } from "file-saver";
 import NavBar from "./components/navBar";
 
