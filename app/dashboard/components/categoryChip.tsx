01af33d76719a18f67b06c8ed3b6f80485dd8163
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-03-25 00:29:53 +0700

    added: category chip

---
+++ app/dashboard/components/categoryChip.tsx
@@ -0,0 +1,11 @@
+interface CategoryChipProps {
+    category: string;
+  }
+  
+  export default function CategoryChip({ category }: CategoryChipProps) {
+    return (
+      <div className="badge badge-primary badge-outline">
+        {category}
+      </div>
+    );
+  }
