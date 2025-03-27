43812655f2528799da3a4c2a2562b78f6cccceac
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-03-27 10:34:10 +0700

    added: toast

--- components/toast.tsx
+++ components/toast.tsx
@@ -32,7 +32,7 @@ export const Toast = ({ messages, duration = 2000, onClose }: ToastProps) => {
         />
       ))}
     </div>,
-    document.body,
+    document.body
   );
 };
 
@@ -113,22 +113,22 @@ export const ToastProvider = ({
 
   const showSuccessToast = useCallback(
     (message: string) => showToast(message, "success"),
-    [showToast],
+    [showToast]
   );
 
   const showErrorToast = useCallback(
     (message: string) => showToast(message, "error"),
-    [showToast],
+    [showToast]
   );
 
   const showInfoToast = useCallback(
     (message: string) => showToast(message, "info"),
-    [showToast],
+    [showToast]
   );
 
   const showWarningToast = useCallback(
     (message: string) => showToast(message, "warning"),
-    [showToast],
+    [showToast]
   );
 
   return (
