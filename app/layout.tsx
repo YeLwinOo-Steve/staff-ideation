90d89919852073cff43f7e1b4ba9ae66bea7e305
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-03-27 10:33:04 +0700

    updated: theme provider and text base color

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google";
 import { ToastProvider } from "@/components/toast";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { ThemeProvider } from "next-themes";
+import ThemeWrapper from "./themeWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,17 +14,38 @@ export const metadata = {
   description: "EWSD Project using Zustand & Zod",
 };
 
+const ThemeProviderClient = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="data-theme"
+      defaultTheme="lemonade"
+      storageKey="data-theme"
+      themes={["lemonade", "abyss"]}
+      enableSystem={false}
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html data-theme="lemonade" lang="en">
-      <body className={inter.className} suppressHydrationWarning>
-        <ToastProvider>{children}</ToastProvider>
-        <Analytics />
-        <SpeedInsights />
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-base-100 text-base-content`}
+        suppressHydrationWarning
+      >
+        <ThemeProviderClient>
+          <ThemeWrapper />
+          <ToastProvider>{children}</ToastProvider>
+          <Analytics />
+          <SpeedInsights />
+        </ThemeProviderClient>
       </body>
     </html>
   );
