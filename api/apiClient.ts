f58ab9c0d102525cacfa254e8a9756b5b614259f
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-04-22 01:18:58 +0700

    added: interceptor

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useAuthStore } from "@/store/authStore";
+import { redirect } from "next/navigation";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -8,13 +10,26 @@ const apiClient = axios.create({
   },
 });
 
+// request interceptor
+apiClient.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// response interceptor to handle unauthorized requests
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    const message = error.response?.data?.message || "Something went wrong";
-
+    const UNAUTHENTICATED = 401;
+    if (error.response?.status === UNAUTHENTICATED) {
+      useAuthStore.getState().logout();
+      redirect("/login");
+    }
     return Promise.reject(error);
-  },
+  }
 );
 
 export default apiClient;
