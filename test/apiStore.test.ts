72122f1949297a31060c20d1e13e829cc5fe307b
Ye Lwin Oo <yelwinoo.work@gmail.com>
2025-05-03 21:02:41 +0700

    added: more api tests

--- test/apiStore.test.ts
+++ test/apiStore.test.ts
@@ -6,19 +6,35 @@ import {
   mockCategory,
   mockSystemSetting,
   mockPaginatedResponse,
+  mockUser,
+  mockComment,
+  mockUserLog,
+  mockReportedIdea,
+  mockReportDetail,
+  mockReportedUser,
+  mockHiddenIdea,
+  mockBannedUser,
+  mockActiveUser,
+  mockDepartmentReport,
+  mockAnonymousIdea,
+  mockAnonymousComment,
+  mockLoginActivity,
 } from "./mockData";
 
-// Mock the entire repository
+// Mock the entire repository module
 jest.mock("@/api/repository", () => ({
   departmentApi: {
     getAll: () => Promise.resolve({ data: { data: [mockDepartment] } }),
-    create: () => Promise.resolve({ data: { data: mockDepartment } }),
+    create: () => Promise.resolve({ data: mockDepartment }),
+    getUsers: () => Promise.resolve({ data: { data: [mockUser] } }),
+    update: () => Promise.resolve({ data: mockDepartment }),
+    delete: () => Promise.resolve({ data: { message: "Deleted" } }),
   },
   ideaApi: {
     getAll: () =>
       Promise.resolve({
         data: {
-          data: [mockIdea],
+          data: [{ ...mockIdea, is_enabled: true }],
           meta: { current_page: 1, last_page: 1, total: 1 },
         },
       }),
@@ -31,14 +47,104 @@ jest.mock("@/api/repository", () => ({
           meta: { current_page: 1, last_page: 1, total: 1 },
         },
       }),
+    getOne: () => Promise.resolve({ data: { data: mockIdea } }),
+    update: () => Promise.resolve({ data: mockIdea }),
+    delete: () => Promise.resolve({ data: { message: "Deleted" } }),
+    updateCategory: () => Promise.resolve({ data: mockIdea }),
   },
   categoryApi: {
     getAll: () => Promise.resolve({ data: { data: [mockCategory] } }),
-    create: () => Promise.resolve({ data: { data: mockCategory } }),
+    create: () => Promise.resolve({ data: mockCategory }),
+    update: () => Promise.resolve({ data: mockCategory }),
+    delete: () => Promise.resolve({ data: { message: "Deleted" } }),
   },
   systemSettingApi: {
     getAll: () => Promise.resolve({ data: { data: [mockSystemSetting] } }),
     update: () => Promise.resolve({ data: mockSystemSetting }),
+    create: () => Promise.resolve({ data: mockSystemSetting }),
+    delete: () => Promise.resolve({ data: { message: "Deleted" } }),
+    getCSV: () => Promise.resolve(new Blob()),
+  },
+  commentApi: {
+    getCommentsForIdea: () => Promise.resolve({ data: { data: [mockComment] } }),
+    create: () => Promise.resolve({ data: mockComment }),
+    update: () => Promise.resolve({ data: mockComment }),
+    delete: () => Promise.resolve({ data: { message: "Deleted" } }),
+  },
+  voteApi: {
+    create: () => Promise.resolve({ data: { message: "Vote created" } }),
+  },
+  userApi: {
+    getAll: () => Promise.resolve({ 
+      data: { 
+        data: [mockUser],
+        meta: { current_page: 1, last_page: 1, total: 1 }
+      }
+    }),
+    getOne: () => Promise.resolve({ data: { data: mockUser } }),
+    create: () => Promise.resolve({ data: mockUser }),
+    update: () => Promise.resolve({ data: mockUser }),
+  },
+  roleApi: {
+    getAll: () => Promise.resolve({ data: { data: [{ id: 1, name: "Admin" }] } }),
+  },
+  reportApi: {
+    create: () => Promise.resolve({ data: { message: "Report created" } }),
+    reportIdea: () => Promise.resolve({ data: { message: "Report created" } }),
+    getReportedIdeas: () => Promise.resolve({ 
+      data: { 
+        data: [mockReportedIdea],
+        meta: { current_page: 1, last_page: 1, total: 1 }
+      }
+    }),
+    getReportDetails: () => Promise.resolve({ data: [mockReportDetail] }),
+    getReportedUsers: () => Promise.resolve({ data: [mockReportedUser] }),
+    getUserReportedIdeas: () => Promise.resolve({ data: { data: [mockReportedIdea] } }),
+  },
+  hideApi: {
+    hideIdea: () => Promise.resolve({ data: { message: "Idea hidden" } }),
+    getHiddenIdeas: () => Promise.resolve({ data: [mockHiddenIdea] }),
+    hideUserIdeas: () => Promise.resolve({ data: { message: "Ideas hidden" } }),
+    getHiddenUsers: () => Promise.resolve({ 
+      data: { 
+        data: [mockHiddenIdea],
+        meta: { current_page: 1, last_page: 1, total: 1 }
+      }
+    }),
+  },
+  permissionApi: {
+    removeIdeaPermissions: () => Promise.resolve({ data: { message: "Permissions removed" } }),
+    giveIdeaPermissions: () => Promise.resolve({ data: { message: "Permissions given" } }),
+    getBannedUsers: () => Promise.resolve({ 
+      data: { 
+        data: [mockBannedUser],
+        meta: { current_page: 1, last_page: 1, total: 1 }
+      }
+    }),
+  },
+  reportingApi: {
+    getActiveUsers: () => Promise.resolve({ data: [mockActiveUser] }),
+    getDepartmentReport: () => Promise.resolve({ data: [mockDepartmentReport] }),
+    getAnonymousIdeas: () => Promise.resolve({ 
+      data: { 
+        data: [mockAnonymousIdea],
+        meta: { current_page: 1, last_page: 1, total: 1 }
+      }
+    }),
+    getAnonymousComments: () => Promise.resolve({ 
+      data: { 
+        data: [mockAnonymousComment],
+        meta: { current_page: 1, last_page: 1, total: 1 }
+      }
+    }),
+  },
+  loginActivityApi: {
+    getAll: () => Promise.resolve({ 
+      data: { 
+        data: [mockLoginActivity],
+        meta: { current_page: 1, last_page: 1, total: 1 }
+      }
+    }),
   },
 }));
 
@@ -90,19 +196,19 @@ describe("API Store", () => {
     });
 
     it("should fetch ideas with params", async () => {
-      const params = { department: "1", latest: "true" };
+      const params = { page: "1" };
       await useApiStore.getState().fetchIdeas(params);
 
       const store = useApiStore.getState();
-      expect(store.ideas).toEqual([mockIdea]);
-      expect(store.ideaPagination.data).toEqual([mockIdea]);
+      expect(store.ideas).toEqual([{ ...mockIdea, is_enabled: true }]);
+      expect(store.ideaPagination.data).toEqual([{ ...mockIdea, is_enabled: true }]);
       expect(store.error).toBeNull();
     });
 
     it("should create idea and refresh list", async () => {
       const formData = new FormData();
       await useApiStore.getState().createIdea(formData);
-      expect(useApiStore.getState().ideas).toEqual([mockIdea]);
+      expect(useApiStore.getState().ideas).toEqual([{ ...mockIdea, is_enabled: true }]);
     });
     it("should submit idea and refresh list", async () => {
       await useApiStore.getState().submitIdea(1);
@@ -201,3 +307,219 @@ describe("API Store", () => {
     });
   });
 });
+
+describe("User Management", () => {
+  beforeEach(() => {
+    useApiStore.setState({
+      users: [],
+      user: null,
+      userPagination: {
+        data: [],
+        currentPage: 1,
+        lastPage: 1,
+        total: 0,
+        loading: false,
+      },
+    });
+  });
+
+  it("should fetch users with pagination", async () => {
+    await useApiStore.getState().fetchUsers(1);
+    const store = useApiStore.getState();
+    expect(store.userPagination.data).toEqual([mockUser]);
+    expect(store.userPagination.currentPage).toBe(1);
+    expect(store.userPagination.lastPage).toBe(1);
+    expect(store.userPagination.total).toBe(1);
+  });
+
+  it("should get single user", async () => {
+    const user = await useApiStore.getState().getUser(1);
+    expect(user).toEqual(mockUser);
+    expect(useApiStore.getState().user).toEqual(mockUser);
+  });
+
+  it("should create user", async () => {
+    const formData = new FormData();
+    await useApiStore.getState().createUser(formData);
+    const store = useApiStore.getState();
+    expect(store.userPagination.data).toEqual([mockUser]);
+  });
+
+  it("should update user", async () => {
+    const formData = new FormData();
+    await useApiStore.getState().updateUser(1, formData);
+    const store = useApiStore.getState();
+    expect(store.userPagination.data).toEqual([mockUser]);
+    expect(store.error).toBeNull();
+  });
+});
+
+describe("Comments", () => {
+  beforeEach(() => {
+    useApiStore.setState({ comments: [] });
+  });
+
+  it("should get comments for idea", async () => {
+    await useApiStore.getState().getCommentsForIdea(1);
+    expect(useApiStore.getState().comments).toEqual([mockComment]);
+  });
+
+  it("should create comment", async () => {
+    const formData = new FormData();
+    await useApiStore.getState().createComment(formData);
+    expect(useApiStore.getState().comments).toEqual([mockComment]);
+  });
+
+  it("should update comment", async () => {
+    const formData = new FormData();
+    await useApiStore.getState().updateComment(1, formData);
+    expect(useApiStore.getState().comments).toEqual([mockComment]);
+  });
+
+  it("should delete comment", async () => {
+    await useApiStore.getState().deleteComment(1);
+    expect(useApiStore.getState().comments).toEqual([]);
+  });
+});
+
+describe("Voting", () => {
+  it("should create vote", async () => {
+    await useApiStore.getState().createVote(1, 1);
+    expect(useApiStore.getState().error).toBeNull();
+  });
+});
+
+describe("Reporting System", () => {
+  beforeEach(() => {
+    useApiStore.setState({
+      reportedIdeas: {
+        data: [],
+        currentPage: 1,
+        lastPage: 1,
+        total: 0,
+        loading: false,
+      },
+      reportDetails: [],
+      reportedUsers: [],
+    });
+  });
+
+  it("should report idea", async () => {
+    const formData = new FormData();
+    await useApiStore.getState().reportIdea(formData);
+    expect(useApiStore.getState().error).toBeNull();
+  });
+
+  it("should fetch reported ideas", async () => {
+    await useApiStore.getState().fetchReportedIdeas(1);
+    expect(useApiStore.getState().reportedIdeas.data).toEqual([mockReportedIdea]);
+  });
+
+  it("should fetch report details", async () => {
+    await useApiStore.getState().fetchReportDetails(1);
+    expect(useApiStore.getState().reportDetails).toEqual([mockReportDetail]);
+  });
+
+  it("should fetch reported users", async () => {
+    await useApiStore.getState().fetchReportedUsers();
+    expect(useApiStore.getState().reportedUsers).toEqual([mockReportedUser]);
+  });
+
+  it("should fetch user reported ideas", async () => {
+    await useApiStore.getState().fetchUserReportedIdeas(1);
+    expect(useApiStore.getState().reportedIdeas.data).toEqual([mockReportedIdea]);
+  });
+});
+
+describe("Content Moderation", () => {
+  beforeEach(() => {
+    useApiStore.setState({
+      hiddenIdeas: [],
+      hiddenUsers: {
+        data: [],
+        currentPage: 1,
+        lastPage: 1,
+        total: 0,
+        loading: false,
+      },
+    });
+  });
+
+  it("should get hidden ideas", async () => {
+    await useApiStore.getState().getHiddenIdeas();
+    expect(useApiStore.getState().hiddenIdeas).toEqual([mockHiddenIdea]);
+  });
+
+  it("should hide user ideas", async () => {
+    await useApiStore.getState().hideUserIdeas(1, 1);
+    expect(useApiStore.getState().error).toBeNull();
+  });
+
+  it("should get hidden users", async () => {
+    await useApiStore.getState().getHiddenUsers();
+    expect(useApiStore.getState().hiddenUsers.data).toEqual([]);
+  });
+
+  it("should manage user permissions", async () => {
+    await useApiStore.getState().removeIdeaPermissions(1);
+    expect(useApiStore.getState().error).toBeNull();
+
+    await useApiStore.getState().giveIdeaPermissions(1);
+    expect(useApiStore.getState().error).toBeNull();
+  });
+
+  it("should get banned users", async () => {
+    await useApiStore.getState().getBannedUsers();
+    expect(useApiStore.getState().bannedUsers.data).toEqual([mockBannedUser]);
+  });
+});
+
+describe("Analytics and Reporting", () => {
+  beforeEach(() => {
+    useApiStore.setState({
+      activeUsers: [],
+      departmentReport: [],
+      anonymousIdeas: {
+        data: [],
+        currentPage: 1,
+        lastPage: 1,
+        total: 0,
+        loading: false,
+      },
+      anonymousComments: {
+        data: [],
+        currentPage: 1,
+        lastPage: 1,
+        total: 0,
+        loading: false,
+      },
+    });
+  });
+
+  it("should get active users", async () => {
+    await useApiStore.getState().getActiveUsers();
+    expect(useApiStore.getState().activeUsers).toEqual([mockActiveUser]);
+  });
+
+  it("should get department report", async () => {
+    await useApiStore.getState().getDepartmentReport();
+    expect(useApiStore.getState().departmentReport).toEqual([mockDepartmentReport]);
+  });
+
+  it("should get anonymous ideas", async () => {
+    await useApiStore.getState().getAnonymousIdeas();
+    expect(useApiStore.getState().anonymousIdeas.data).toEqual([mockAnonymousIdea]);
+  });
+
+  it("should get anonymous comments", async () => {
+    await useApiStore.getState().getAnonymousComments();
+    expect(useApiStore.getState().anonymousComments.data).toEqual([mockAnonymousComment]);
+  });
+});
+
+// describe("Login Activity", () => {
+//   it("should get user login activities", async () => {
+//     await useApiStore.getState().getUserLoginActivities(1);
+//     expect(useApiStore.getState().loginActivities).toEqual([mockLoginActivity]);
+//   });
+// });
--- test/mockData.ts
+++ test/mockData.ts
@@ -1,56 +1,189 @@
-export const mockDepartment = {
+import {
+  Department,
+  User,
+  Idea,
+  Category,
+  SystemSetting,
+  Comment,
+  UserLog,
+  ReportedIdea,
+  ReportDetail,
+  ReportedUser,
+  HiddenIdea,
+  BannedUser,
+  ActiveUser,
+  DepartmentReport,
+  AnonymousIdea,
+  AnonymousComment,
+  LoginActivity,
+} from "@/api/models";
+
+export const mockDepartment: Department = {
   id: 1,
-  department_name: "Computer Science",
+  department_name: "Engineering",
+  qa_coordinator_name: "John Doe",
+  created_at: "2024-01-01",
+  updated_at: "2024-01-01",
   QACoordinatorID: 1,
 };
 
-export const mockUser = {
+export const mockUser: User = {
   id: 1,
-  role_id: 2,
-  permissions_id: "1,2,3",
-  name: "John Doe",
-  email: "john@example.com",
-  department_id: 1,
-  photo: "https://example.com/photo.jpg",
+  name: "Test User",
+  email: "test@example.com",
+  roles: ["staff"],
+  permissions: ["create_idea", "view_idea"],
+  department: "Engineering",
 };
 
-export const mockIdea = {
+export const mockIdea: Idea = {
   id: 1,
-  title: "Improve Campus WiFi",
-  content: "We should upgrade our WiFi infrastructure",
+  title: "Test Idea",
+  content: "Test Content",
   is_anonymous: false,
-  is_enabled: true,
+  category: ["Innovation"],
+  system_setting_id: 1,
+  created_at: "2024-01-01",
+  updated_at: "2024-01-01",
+};
+
+export const mockCategory: Category = {
+  id: 1,
+  name: "Innovation",
+  created_at: "2024-01-01",
+  updated_at: "2024-01-01",
+};
+
+export const mockSystemSetting: SystemSetting = {
+  id: 1,
+  idea_closure_date: "2024-12-31",
+  final_closure_date: "2025-01-31",
+  academic_year: "2024",
+  status: 1,
+};
+
+export const mockComment: Comment = {
+  id: 1,
   user_id: 1,
-  category: 1,
-  document: [
-    {
-      file_name: "proposal.pdf",
-      file_path: "/documents/proposal.pdf",
-    },
-  ],
+  idea_id: 1,
+  comment: "Test Comment",
+  is_anonymous: false,
+  is_enabled: 1,
+  system_setting_id: 1,
+  created_at: "2024-01-01",
+  updated_at: "2024-01-01",
+  user_name: "Test User",
+  user_photo: "photo.jpg",
+  department: "Engineering",
+};
+
+export const mockUserLog: UserLog = {
+  user: "Test User",
+  email: "test@example.com",
+  photo: "photo.jpg",
+  type: "idea",
+  action: "create",
+  activity: "Created a new idea",
+  time: "2024-01-01",
+};
+
+export const mockReportedIdea: ReportedIdea = {
+  id: 1,
+  user_id: 1,
+  title: "Reported Idea",
+  content: "Reported Content",
+  user_name: "Test User",
+  photo: "photo.jpg",
+  department: "Engineering",
+  no_of_report: 1,
+  hidden: 0,
+};
+
+export const mockReportDetail: ReportDetail = {
+  reason: "Inappropriate content",
+  user_name: "Reporter",
+  user_photo: "photo.jpg",
+  user_department: "Engineering",
+};
+
+export const mockReportedUser: ReportedUser = {
+  name: "Reported User",
+  photo: "photo.jpg",
+  email: "reported@example.com",
+  department: "Engineering",
+  hidden: 0,
+  no_of_reports: 1,
+  banned: false,
+};
+
+export const mockHiddenIdea: HiddenIdea = {
+  ...mockIdea,
+  hidden: 1,
+};
+
+export const mockBannedUser: BannedUser = {
+  ...mockUser,
+  hidden: 1,
+  email_verified_at: "2024-01-01",
+  department_id: 1,
+};
+
+export const mockActiveUser: ActiveUser = {
+  id: 1,
+  name: "Active User",
+  photo: "photo.jpg",
+  email: "active@example.com",
+  department_id: 1,
+  email_verified_at: "2024-01-01",
+  hidden: 0,
+  created_at: "2024-01-01",
+  updated_at: "2024-01-01",
+  ideas_count: 5,
+  comments_count: 10,
+  total_activity: 15,
+};
+
+export const mockDepartmentReport: DepartmentReport = {
+  ...mockDepartment,
+  qa_coordinator: "John Doe",
+  total_ideas: 10,
+  total_comments: 20,
+  total_user: 5,
+  ideas_percentage: 50,
+  contributors: 3,
+  total_activity: 30,
+};
+
+export const mockAnonymousIdea: AnonymousIdea = {
+  id: 1,
+  title: "Anonymous Idea",
+  content: "Anonymous Content",
+  created_at: "2024-01-01",
+  department: "Engineering",
+};
+
+export const mockAnonymousComment: AnonymousComment = {
+  id: 1,
+  comment: "Anonymous Comment",
+  created_at: "2024-01-01",
+  idea_title: "Test Idea",
+  department: "Engineering",
+};
+
+export const mockLoginActivity: LoginActivity = {
+  id: 1,
+  user_id: 1,
+  browser: "Chrome",
+  ip_address: "127.0.0.1",
+  created_at: "2024-01-01",
+  updated_at: "2024-01-01",
 };
 
 export const mockPaginatedResponse = {
-  data: {
-    data: [mockIdea],
-    meta: {
-      current_page: 1,
-      last_page: 1,
-      total: 1,
-    },
+  data: [],
+  meta: {
+    current_page: 1,
+    last_page: 1,
+    total: 0,
   },
-};
-
-export const mockCategory = {
-  id: 1,
-  name: "Infrastructure",
-};
-
-export const mockSystemSetting = {
-  id: 1,
-  idea_closure_date: "2024-03-01",
-  final_closure_date: "2024-03-15",
-  academic_year: "2024",
-  status: 1,
-  total_ideas: 10,
 };
